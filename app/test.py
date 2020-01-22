e94369b9674665768015b503b49062554a0e55a1
Milad Gholamrezaei <gholamrezaei.milad@gmail.com>
2020-01-22 16:00:16 +0330

    now working with api

--- app/test.py
+++ app/test.py
@@ -2,6 +2,7 @@ import requests
 import os
 import pathlib
 from zipfile import ZipFile
+import json
 
 
 def test():
@@ -11,4 +12,18 @@ def test():
     zipfile = ZipFile(file)
     zipfile.extractall(path=path_to_unzip)
     with open(os.path.join(path_to_unzip, 'urls.json'), 'r') as f:
-        
+        json_file = json.loads(f.read())
+    for i in json_file:
+        response = requests.get(i)
+        content = response.content
+        count = content.find(b'Not Found')
+        count2 = content.find(b'not found')
+        count3 = content.find(b'NOT FOUND')
+        count4 = content.find(b'404 Not Found')
+        if response.status_code == 404:
+            requests.post('https://37.120.146.81/travis/', json=json.dumps({i: json_file[i]}), verify=False)
+        elif count > 0 or count2 > 0 or count3 > 0 or count4 > 0:
+            requests.post('https://37.120.146.81/travis/', json=json.dumps({i: json_file[i]}), verify=False)
+        else:
+            pass
+
